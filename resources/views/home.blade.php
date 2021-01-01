3406e93904f07810243fc69db9c67f76c67565f3
aliharby12 <aliharby565@gmail.com>
2021-01-01 09:07:52 +0200

    add a default image to products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
         @foreach ($products as $product)
         <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <a href="#"><img class="card-img-top" src="/img/cart.jpg" alt=""></a><hr>
               <div class="card-body">
                 <h4 class="card-title">
                   <a href="#">{{$product->name}}</a>
